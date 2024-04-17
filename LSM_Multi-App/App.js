64c859e1f535489c3ea98b17fa7f1bef1b102aeb
LukySand <luky.sand@gmail.com>
2024-04-17 15:02:11 -0300

    added the initial structure for the project

---
+++ LSM_Multi-App/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Esta va a ser la Multi-App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
